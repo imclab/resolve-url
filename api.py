e213bd21efd336fb032c5a66ed0a9145a4603ae9
knightlab <knightlab@northwestern.edu>
2014-01-28 02:06:12 +0000

    Removes unused wsgi file; Changes development port back to Flask-preferred 5000

--- api.py
+++ api.py
@@ -6,32 +6,12 @@ Main entrypoint file.  To run:
 from flask import Flask
 from flask import request
 from flask import jsonify
-#import importlib
-#import sys
-#import os
 from urlresolve import RedisUrlResolver
 import logging
 from logging import StreamHandler
 
-#if __name__ == "__main__":
-#    if not os.environ.get('FLASK_SETTINGS_MODULE', ''):
-#        os.environ['FLASK_SETTINGS_MODULE'] = 'core.settings.loc'
-#
 app = Flask(__name__)
-app.debug = True
 app.logger.addHandler(StreamHandler())
-
-#settings_module = os.environ.get('FLASK_SETTINGS_MODULE')
-
-#try:
-#    importlib.import_module(settings_module)
-#except ImportError, e:
-#    raise ImportError(
-#        "Could not import settings '%s' (Is it on sys.path?): %s" \
-#        % (settings_module, e))
-#
-#settings = sys.modules[settings_module]
-
 
 @app.route('/', methods=['GET'])
 def index():
@@ -44,4 +24,4 @@ def index():
     
         
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
